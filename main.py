70082b3e2e6c7fd98308e3b786c65bdf9db43029
MorganMLG <cholody.kacper1998@gmail.com>
2022-05-18 15:06:46 +0200

    Python requirements

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from scipy.io import loadmat
 import pandas as pd
+from matplotlib import pyplot as plt
 
-# %% MAIN
+DATA_FILE = "a08r.mat"
+ARRAY_NAME = "a08r"
+SPS = 300 # Samples per second
+
 if __name__ == "__main__":
-    data = pd.DataFrame(loadmat("a08r.mat")['a08r'], columns=(["channel1", "channel2", "channel3", "channel4", "channel5"]))
-    print(data)
+    data = pd.DataFrame(loadmat(DATA_FILE)[ARRAY_NAME], columns=(["ch1", "ch2", "ch3", "ch4", "ch5"]))
+    print(data.head(3))    
+    
+    plt.figure(figsize=(25, 15))
+    plt.plot(data.head(300000).rolling(SPS).mean(), label=data.columns)
+    plt.legend()
+    plt.show()
